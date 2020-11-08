5aec0adbebea4f2502f6d963095785185711996d
kurshanyWaleed <kurshany.waleed@gmail.com>
2020-11-08 18:35:17 +0100

    v0.0

---
+++ Location_voiture/src/test/java/com/dmwm/location_voiture/Location_voitureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmwm.location_voiture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Location_voitureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
